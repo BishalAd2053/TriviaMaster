7d80b0fd7f013e35197b94529f75aad192d0386d
BishalAd2053 <bishaladhikari053@gmail.com>
2025-01-03 14:04:53 -0500

    Microservices creaded

---
+++ game-logic-service/game-logic-service/src/main/java/com/triviamaster/game_logic_service/GameLogicServiceApplication.java
@@ -0,0 +1,13 @@
+package com.triviamaster.game_logic_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLogicServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLogicServiceApplication.class, args);
+	}
+
+}
---
+++ game-logic-service/game-logic-service/src/test/java/com/triviamaster/game_logic_service/GameLogicServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triviamaster.game_logic_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLogicServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
